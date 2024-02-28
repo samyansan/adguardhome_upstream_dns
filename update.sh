316b58d8d0d7b1e7f6772acb48c3958efcf74c37
samyansan <41437406+samyansan@users.noreply.github.com>
2024-02-28 13:18:09 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,20 +1,15 @@
-rm /opt/AdGuardHome/upstream_dns_file
+rm /opt/AdGuardHome/upstream_dns_file.txt
 
-curl  "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt"  >> /opt/AdGuardHome/upstream_dns_file
+curl  "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt"  >> /opt/AdGuardHome/upstream_dns_file.txt
 
-sed "s/^full://g;/^regexp:.*$/d;s/^/[\//g;s/$/\/]tls:\/\/120.53.53.53/g" -i /opt/AdGuardHome/upstream_dns_file 
-#echo "[/123456.xyz/]tls://120.53.53.53" >> /opt/AdGuardHome/upstream_dns_file
-#echo "[/rfi.fr/]tcp://8.8.8.8" >> /opt/AdGuardHome/upstream_dns_file
-#echo "tcp://8.8.8.8" >> /opt/AdGuardHome/upstream_dns_file
-#echo "tcp://8.8.4.4" >> /opt/AdGuardHome/upstream_dns_file
+sed "s/^full://g;/^regexp:.*$/d;s/^/[\//g;s/$/\/]tls:\/\/dns.alidns.com/g" -i /opt/AdGuardHome/upstream_dns_file.txt
+#echo "[/123456.xyz/]tls://dns.alidns.com" >> /opt/AdGuardHome/upstream_dns_file.txt
+#echo "[/rfi.fr/]tcp://8.8.8.8" >> /opt/AdGuardHome/upstream_dns_file.txt
+#echo "tcp://8.8.8.8" >> /opt/AdGuardHome/upstream_dns_file.txt
+#echo "tcp://8.8.4.4" >> /opt/AdGuardHome/upstream_dns_file.txt
 
-echo "https://public.dns.iij.jp/dns-query" >> /opt/AdGuardHome/upstream_dns_file
-echo "https://doh.opendns.com/dns-query" >> /opt/AdGuardHome/upstream_dns_file
-echo "https://doh.dns.sb/dns-query" >> /opt/AdGuardHome/upstream_dns_file
-echo "https://dns.twnic.tw/dns-query" >> /opt/AdGuardHome/upstream_dns_file
-echo "tls://fi.dot.dns.snopyta.org" >> /opt/AdGuardHome/upstream_dns_file
-echo "https://dns.cloudflare.com/dns-query" >> /opt/AdGuardHome/upstream_dns_file
-
-
-systemctl restart AdGuardHome.service
-
+echo "https://dns.pub/dns-query" >> /opt/AdGuardHome/upstream_dns_file.txt
+echo "https://100284.alidns.com/dns-query" >> /opt/AdGuardHome/upstream_dns_file.txt
+echo "tls://doh-62251cfe.dot.pub" >> /opt/AdGuardHome/upstream_dns_file.txt
+echo "tls://dot.360.cn" >> /opt/AdGuardHome/upstream_dns_file.txt
+echo "tls://1dot1dot1dot1.cloudflare-dns.com" >> /opt/AdGuardHome/upstream_dns_file.txt
